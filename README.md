31b531bc51a519e3b7b9d3cc40ee8cbc727619e8
JeffRisberg <j.s.risberg@gmail.com>
2023-10-15 12:25:35 -0700

    built out modules

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LangChain02
+
+Icebreaker
--- app.py
+++ app.py
@@ -1,36 +1,29 @@
-from langchain import PromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
+from flask import Flask, render_template, request, jsonify
+from ice_breaker import ice_break_with
+
+app = Flask(__name__)
 
 
-information = """
-Elon Reeve Musk  is a business magnate and investor. Musk is the founder, chairman,
-CEO and chief technology officer of SpaceX; angel investor, CEO, product
-architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.;
-founder of the Boring Company; co-founder of Neuralink and OpenAI; and president of the Musk Foundation.
-He is the wealthiest person in the world, with an estimated net worth of US$232 billion
-as of September 2023, according to the Bloomberg Billionaires Index, and $253 billion according
- to Forbes, primarily from his ownership stakes in both Tesla and SpaceX.
-"""
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/process", methods=["POST"])
+def process():
+    name = request.form["name"]
+    summary_and_facts, interests, ice_breakers, profile_pic_url = ice_break_with(
+        name=name
+    )
+    return jsonify(
+        {
+            "summary_and_facts": summary_and_facts.to_dict(),
+            "interests": interests.to_dict(),
+            "ice_breakers": ice_breakers.to_dict(),
+            "picture_url": profile_pic_url,
+        }
+    )
+
 
 if __name__ == "__main__":
-    print("Hello LangChain")
-
-    summary_template = """
-      Given the information {information} about a person, I want to you create:
-      1. a short summary of the person
-      2. two interesting facts about the person
-
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables = ["information"], template = summary_template)
-
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-
-    print(chain.run(information=information))
-
-
-
+    app.run(host="0.0.0.0", debug=True)
